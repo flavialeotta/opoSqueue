70e167cee14facf9ac7f05e90acea079336b4d34
flavia_cent <flavia.leotta@hotmail.com>
2026-05-18 16:27:01 +0200

    Fixed base directory path

--- src/oposqueue/core/asset_path.py
+++ src/oposqueue/core/asset_path.py
@@ -1,6 +1,12 @@
 import os
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+import os
+
+
+CORE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+BASE_DIR = os.path.dirname(CORE_DIR)
+
 def get_asset_path(relative_path):
-    """Converts a standard asset path into an absolute system path."""
+    """Converts a standard asset path into an absolute path relative to the package root."""
     return os.path.join(BASE_DIR, relative_path)
